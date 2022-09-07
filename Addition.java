47c90fc1e56d32781f2208534e11f5bb0a133b7b
Dharunya <88583525+dharunya0609@users.noreply.github.com>
2022-09-07 21:06:32 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Addition
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of a and b:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("The sum of "+a+" and "+b+" is "+c);
+		
+		
+	}
+}
