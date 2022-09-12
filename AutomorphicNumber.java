9b1cff6296e201072ab9d680dd03527ede4b9438
Dharunya <88583525+dharunya0609@users.noreply.github.com>
2022-09-12 22:52:48 +0530

    Create AutomorphicNumber.java

---
+++ AutomorphicNumber.java
@@ -0,0 +1,28 @@
+public class AutomorphicNumber 
+{      
+static boolean isAutomorphic(int num)   
+{   
+
+int square = num * num;   
+
+while (num > 0)   
+{   
+//find the remainder (last digit) of the variable num and square and comparing them  
+if (num % 10 != square % 10)   
+//returns false if digits are not equal  
+return false;   
+//reduce num and square by dividing them by 10  
+num = num/10;   
+square = square/10;   
+}   
+return true;   
+}   
+//Driver code  
+public static void main(String args[])   
+{   
+//number to be check      
+//calling the method and prints the result accordingly  
+System.out.println(isAutomorphic(76) ? "Automorphic" : "Not Automorphic");   
+System.out.println(isAutomorphic(13) ? "Automorphic" : "Not Automorphic");   
+}   
+}  
